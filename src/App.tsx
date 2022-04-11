b1cd04553d1b704ecef2207929b629c24e1f15e5
LillyQiLe <467171232@qq.com>
2022-04-11 14:05:45 +0800

    Footer 80%

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
                 content === "score" ?  <Score/> :
                 content === 'result' ? <Result/> :
                 content === 'guide'  ? <Guide/> : <Proportion/>}</Content>
-      <Footer>Footer</Footer>
+      <Footer style={{ textAlign: 'center', position: 'fixed', width: '100%', margin: '520px 0 0 0' }}>Pick Me Up ©2022 Created by Qi Le</Footer>
     </Layout>
   );
 }
